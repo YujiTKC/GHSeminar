9321de9bb238e611cadcd75e160b0440c0e6f0ba
YujiTKC <y083623@gmail.com>
2020-07-02 20:16:07 +0900

    200702

--- GHSeminar/Program.cs
+++ GHSeminar/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+
 namespace GHSeminar
 {
     class Program
@@ -7,6 +8,45 @@ namespace GHSeminar
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string str = Console.ReadLine();
+
+            int n = int.Parse(str);
+
+
+            Console.WriteLine("loop statement");
+
+            for(int i = 0; i<n; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            Console.WriteLine("if statement");
+            for (int i = 0; i<n; i++)
+            {
+                Console.WriteLine(i);
+                if (i > 3)
+                {
+                    Console.WriteLine("i > 3");
+                }
+                else
+                {
+                    Console.WriteLine("i <= 3");
+                }
+            }
+
+            int j, k;
+
+            //if(j == 3) //jが３か
+            //if(j != 3) // jが3じゃないか
+            //if(j <= 3) // jが3以下
+
+
+
         }
+
+
+
     }
 }
---
+++ Semi2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+
+namespace Semi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var a = ReadFile(@"a.txt");
+
+        }
+
+        static int SumAB(int a, int b)
+        {
+            return a + b;
+        }
+
+        static double SumAB(double a, double b)
+        {
+            return a + b;
+        }
+
+        static double Mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double[] numbersToArray(double a1, double a2, double a3)
+        {
+
+            double[] ret = new double[3];
+            ret[0] = a1;
+            ret[1] = a2;
+            ret[2] = a3;
+            return ret;
+        }
+
+        static string[] ReadFile(string filePath)
+        {
+            Console.WriteLine("ReadFile");
+            StreamReader sr = new StreamReader(
+                filePath, Encoding.UTF8);
+
+            string text = sr.ReadToEnd();
+
+            Console.WriteLine(text);
+
+            //char[] splitCharArray = new char[]
+            //{
+            //    '\n'
+            //};
+
+            string newline = Environment.NewLine;
+
+            string[] lines = text.Split(newline);
+            return lines;
+        }
+
+    }
+}
